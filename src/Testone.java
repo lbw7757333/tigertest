d84328936196d89794dc333bce42740e29b75b2f
lbw <lbw7757333@163.com>
2017-01-04 18:02:56 +0800

    my first test

---
+++ src/Testone.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+/**
+ * Created by bowen on 2017/1/4.
+ */
+public class Testone {
+
+    @Test
+    public void testOne(){
+
+        System.out.println("haha");
+
+    }
+
+}
